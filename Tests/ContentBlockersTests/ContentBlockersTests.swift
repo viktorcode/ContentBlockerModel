98a1fe39f419c8ff3489192e2abbd4e9b1ffc2e6
Viktor Chernikov <viktorcode@icloud.com>
2019-07-04 20:06:27 +0300

    More tests

--- Tests/ContentBlockersTests/ContentBlockersTests.swift
+++ Tests/ContentBlockersTests/ContentBlockersTests.swift
@@ -2,36 +2,84 @@ import XCTest
 @testable import ContentBlockers
 
 final class ContentBlockersTests: XCTestCase {
-    let escapedOptimisingDomainPrefix = "^[^:]+:\\/\\/+([^:\\/]+\\\\.)?"
-    let blockRule = ContentBlockingRule(
-        trigger: Trigger(urlFilter: optimisingDomainPrefix + "trc\\.taboola\\.com",
-            urlFilterIsCaseSensitive: true,
-            loadType: [.thirdParty]),
-        action: Action(type: .block))
 
-
-
-    func testSanity() {
+    func testEquality() {
         let rule = ContentBlockingRule(
-            trigger: Trigger(urlFilter: "*."),
+            trigger: Trigger(urlFilter: "*.", urlFilterIsCaseSensitive: true,
+                             resourceType: [.document, .image, .script],
+                             loadType: [.firstParty], urlSelection: .ifDomain(["*doma.in"])),
             action: Action(type: .block))
 
         let another = ContentBlockingRule(
-            trigger: Trigger(urlFilter: "*."),
+            trigger: Trigger(urlFilter: "*.", urlFilterIsCaseSensitive: true,
+                             resourceType: [.document, .image, .script],
+                             loadType: [.firstParty], urlSelection: .ifDomain(["*doma.in"])),
             action: Action(type: .block))
 
         XCTAssertEqual(rule, another)
     }
 
-    func testShouldSerialize() {
+    func testSerialization() {
+        let escapedOptimisingDomainPrefix = #"^[^:]+:\/\/+([^:\/]+\\.)?"#
+        let blockRule = ContentBlockingRule(
+            trigger: Trigger(urlFilter: optimisingDomainPrefix + #"trc\.taboola\.com"#,
+                             urlFilterIsCaseSensitive: true,
+                             loadType: [.thirdParty]),
+            action: Action(type: .block))
+
         let json = JSONEncoder()
         XCTAssertNoThrow(try json.encode(blockRule))
         let string = String(data: try! json.encode(blockRule), encoding: .utf8)!
         XCTAssertTrue(string.contains(escapedOptimisingDomainPrefix))
     }
 
+    func testDeserialization() {
+        let jsonData = #"""
+        {
+          "trigger" : {
+            "url-filter" : "^[^:]+:\/\/+([^:\/]+\\.)?trc\\.taboola\\.com",
+            "url-filter-is-case-sensitive" : true,
+            "load-type" : [
+              "third-party"
+            ]
+          },
+          "action" : {
+            "type" : "block"
+          }
+        }
+        """#
+        let json = JSONDecoder()
+        let data = jsonData.data(using: .utf8)!
+        XCTAssertNoThrow(try json.decode(ContentBlockingRule.self, from: data))
+    }
+
+    func testOrdering() {
+        let blockRule1 = ContentBlockingRule(
+            trigger: Trigger(urlFilter: optimisingDomainPrefix + #"trc\.taboola\.com"#,
+                             urlFilterIsCaseSensitive: true,
+                             loadType: [.thirdParty]),
+            action: Action(type: .block))
+        let blockRule2 = ContentBlockingRule(
+            trigger: Trigger(urlFilter: optimisingDomainPrefix + #"trc\.taboola\.com"#,
+                             loadType: [.thirdParty]),
+            action: Action(type: .block))
+        let hidingRule = ContentBlockingRule(
+            trigger: Trigger(urlFilter: "*."),
+            action: Action(type: .cssDisplayNone, selector: ".big-fat-ad"))
+        let exceptionRule = ContentBlockingRule(
+            trigger: Trigger(urlFilter: "*important.com"),
+            action: Action(type: .ignorePreviousRules))
+
+        var list = [exceptionRule, blockRule1, hidingRule, blockRule2]
+        list.sort()
+        XCTAssertEqual(list.last!, exceptionRule, "Exception must go last")
+        XCTAssertEqual(list.first!, blockRule2, "Less specific ahead of more specific")
+        XCTAssertEqual(list[2], hidingRule, "Hiding goes lower priority")
+    }
+
     static var allTests = [
-        ("Test Sanity", testSanity),
-        ("Should Serialise", testShouldSerialize)
+        ("Should be equal", testEquality),
+        ("Should serialise", testSerialization),
+        ("Should deserialize", testDeserialization)
     ]
 }
